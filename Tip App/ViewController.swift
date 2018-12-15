cf34e141e5139290a2b827afb6df190a2c998ff2
Kevin Chen <kc3585@nyu.edu>
2018-12-15 18:25:21 -0500

    Initial Commit

---
+++ Tip App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip App
+//
+//  Created by Kevin Chen on 12/15/2018.
+//  Copyright © 2018 Kevin Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
