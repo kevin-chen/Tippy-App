c6f11f9f1d790b0092ad8bac3a894ee85f03d368
Kevin Chen <kc3585@nyu.edu>
2018-12-19 12:42:03 -0500

    Update ViewController.swift

--- Tip App/ViewController.swift
+++ Tip App/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.billField.becomeFirstResponder()
     }
     
     override func didReceiveMemoryWarning() {
